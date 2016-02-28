7cad7f30709f131007255630770fcdd6342a639f
wookkii <wookkii@home>
2016-02-28 23:27:48 +1100

    In progress - add cat selector for 5+ cats

--- js/cat-clicker.js
+++ js/cat-clicker.js
@@ -1,32 +1,54 @@
-// TWO CATS - CLOSURE
 var cats = [
 	{
-			name: 'Kitty',
-			imgURL : '/img/kitten1.jpg',
-			clickCounter: 0
-		}
-	,
+		name: 'Kitty',
+		imgURL : '/img/kitten1.jpg',
+		clickCounter: 0
+	}	,
 	{
-			name: 'Pussy',
-			imgURL : '/img/kitten2.jpg',
-			clickCounter : 0
-		}
+		name: 'Pussy',
+		imgURL : '/img/kitten2.jpg',
+		clickCounter : 0
+	},
+	{
+		name: 'Tiger and Lilly',
+		imgURL : '/img/kitten3.jpg',
+		clickCounter : 0
+	},
+	{
+		name: 'Bennie',
+		imgURL : '/img/kitten4.jpg',
+		clickCounter : 0
+	},
+	{
+		name: 'Rasputin',
+		imgURL : '/img/kitten5.jpg',
+		clickCounter : 0
+	}
 ];
 
 var noOfCats = cats.length;
 var catContainer;
 
+var catSelectorHTML;
+
 for (var i = 0; i < noOfCats; i++) {
-	catContainer = $('#cat-container' + (i+1));
-	catContainer.find('.cat-heading').text(cats[i].name);
+	// create elements for selectors based on cat array
+	// add onclick event to these elements that will display the cat details in the container area
+	// keep the onclick event on the image of the car to increment counter
+
 	
-	catContainer.click( (function(iCopy) {
-		return function() {
-			var nextCounter = cats[iCopy].clickCounter + 1
-			cats[iCopy].clickCounter = nextCounter;
-			$(this).find('.cat-counter').text('You have clicked me ' + nextCounter  + ' times.');
-		}
-	})(i));
+	// catSelector 
+
+	// catContainer = $('#cat-container' + (i+1));
+	// catContainer.find('.cat-heading').text(cats[i].name);
+	
+	// catContainer.click( (function(iCopy) {
+	// 	return function() {
+	// 		var nextCounter = cats[iCopy].clickCounter + 1
+	// 		cats[iCopy].clickCounter = nextCounter;
+	// 		$(this).find('.cat-counter').text('You have clicked me ' + nextCounter  + ' times.');
+	// 	}
+	// })(i));
 }
 
 
